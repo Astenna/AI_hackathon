1b2798eb5a0c3dfc3285ba529e3c59d27377dcd3
Maciej Szulik <maciej.szulik@intel.com>
2019-09-04 19:51:21 +0200

    fire is coming

--- stock.py
+++ stock.py
@@ -1,4 +1,7 @@
 import yfinance as yf
+import pandas as pd 
+import json
+import numpy as np
 from datetime import date
 
 def fetchDataFromPeriod(start_arg, end_arg):
@@ -46,29 +49,55 @@ def extendDataFrameByResult(dataFrame):
 def dropDataFrameColumns(dataFrame):
     return dataFrame.drop(columns=['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'])
 
-def saveStockDataFrameToCsv(dataFrame):
-    fileName = "stock_data_" + date.today().strftime("%d-%m-%Y") + ".csv"
+def saveDataFrameToCsv(dataFrame, prefix):
+    fileName = prefix+ "_" + date.today().strftime("%d-%m-%Y") + ".csv"
     dataCsvFile = open(fileName, "w")
     dataCsvFile.write(dataFrame.to_csv())
     dataCsvFile.close()
 
+def readCsvfFile(fileName):
+    return pd.read_csv(fileName, sep = ",")
+
+def mergeTwitterStockDataFrames(twitterDataFrame, stockDataFrame):
+    stockArr = list(stockDataFrame.values)
+    # print(stockArr)
+    with open("twitter.json", 'r') as json_file:
+        twitterDict = json.load(json_file)
+
+    for row in stockArr:
+        row.append(twitterDict.get(str(row[0]).split(' ')[0], []))
+        print(row)
+
+    return stockArr
+    # indexesToDrop = []
+    # for stockIndex, stockRow in stockDataFrame.iterrows():
+    #     found = False
+    #     for twitterIndex, twitterRow in twitterDataFrame.iterrows():
+    #         print(twitterRow["Date"], stockRow["Date"])
+    #         if(twitterRow["Date"] == stockRow["Date"]):
+    #             stockRow.set_value(stockIndex, "P1", twitterRow["P1"])
+    #             stockRow.set_value(stockIndex, "P2", twitterRow["P2"])
+    #             stockRow.set_value(stockIndex, "P3", twitterRow["P3"])
+    #             stockRow.set_value(stockIndex, "P4", twitterRow["P4"])
+    #             found = True
+    #     if(found == False):
+    #         indexesToDrop.append(stockIndex)
+    
+    # stockDataFrame = stockDataFrame.drop(index = indexesToDrop)
+    # return stockDataFrame
+
+
 def main():
-    stockDataFrame = fetchDataFromPredefinedPeriod("1y")
+    stockDataFrame = fetchDataFromPredefinedPeriod("5d")
     stockDataFrame = stockDataFrame.reset_index()
     stockDataFrame = extendDataFrameByP5(stockDataFrame)
     stockDataFrame = extendDataFrameByP6(stockDataFrame)
     stockDataFrame = extendDataFrameByP7(stockDataFrame)
     stockDataFrame = extendDataFrameByResult(stockDataFrame)
     stockDataFrame = dropDataFrameColumns(stockDataFrame)
-    saveStockDataFrameToCsv(stockDataFrame)
-
-    # stockDataFrame = extendDataFrameByOpenCloseDerivatives(stockDataFrame.reset_index())
-    # print(stockDataFrame)
-    # stockDataFrame = extendDataFrameByCloseToMaxRatio(stockDataFrame)
-    # stockDataFrame = extendDataFrameByOpenCloseDerivative(stockDataFrame)
-    # stockDataFrame = extendDataFrameByValuesVariety(stockDataFrame)
-
-    # saveStockDataFrameToCsv(stockDataFrame)
+    twitterDataFrame = readCsvfFile("twitter_data.csv")
+    merged = mergeTwitterStockDataFrames(twitterDataFrame, stockDataFrame)
+    print(merged)
 
 if __name__ == "__main__":
     main()
