dfb0d6466d8dc5f6d672c11282d64dd79bbb9839
mitruska <katarzyna.mitrus@intel.com>
2019-09-04 14:28:25 +0200

    Website generator init

---
+++ website-generator/generator.py
@@ -0,0 +1,34 @@
+import csv
+import json
+import os
+
+from collections import OrderedDict
+from datetime import datetime
+from jinja2 import Environment, PackageLoader, select_autoescape
+
+
+def _generate_page(template, output_dir, name, **template_args):
+    page = template.render(template_args)
+
+    # Save static page to file
+    with open(os.path.join(output_dir, name), "w") as f:
+        f.write(page)
+
+
+if __name__ == "__main__":
+    # Website
+    # Create Jinja2 templates environment
+    database = OrderedDict()
+
+    env = Environment(
+        loader=PackageLoader("templates-module", "templates"),
+        autoescape=select_autoescape(["html"]),
+    )
+
+    template = env.get_template("index.html")
+    _generate_page(
+        template,
+         "./docs",
+        "index.html",
+        database=database,
+    )
