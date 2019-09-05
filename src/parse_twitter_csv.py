4330a291cfa043b84537581db5a810dba511ef45
mitruska <katarzyna.mitrus@intel.com>
2019-09-05 11:29:38 +0200

    All twitter data parser

---
+++ src/parse_twitter_csv.py
@@ -0,0 +1,32 @@
+import csv
+import json
+
+
+def read_twitter_csv(csv_path="datasets/training/support/output_got.csv"):
+    with open(csv_path) as csv_file:
+        csvReader = csv.DictReader(
+            csv_file,
+            delimiter=";",
+            fieldnames=(
+                "username",
+                "date",
+                "retweets",
+                "favorites",
+                "text",
+                "geo",
+                "mentions",
+                "hashtags",
+                "id",
+                "permalink",
+            ),
+        )
+        next(csv_file)  # Skip headers
+        out = [row for row in csvReader]
+
+    # write the data to the json file
+    with open("datasets/training/support/twitter_converted.json", "w") as jsonFile:
+        json.dump(out, jsonFile, indent=4)
+
+
+if __name__ == "__main__":
+    read_twitter_csv()
