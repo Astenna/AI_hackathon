a60b720812a83669e305281127cfbae5252ee1f2
mitruska <katarzyna.mitrus@intel.com>
2019-09-05 13:00:38 +0200

    All data prepared

--- src/merge_tweet_stock.py
+++ src/merge_tweet_stock.py
@@ -17,12 +17,12 @@ def merge_data_files(twitter_file, stock_file, output_file):
         reader = csv.reader(csv_file)
         next(reader)  # Skip row with headers
         for stock_row in reader:
-            date = stock_row[1]
+            date = stock_row[2]
             twitter_row = twitter_dict.get(date, [0 for x in range(4)])
             # Append only if twitter_row for this date is not empty
             if twitter_row:
                 twitter_row[0] /= max_tweets  # Also special for Adam
-                all_data_row = [date] + twitter_row + stock_row[2:]
+                all_data_row = [date] + twitter_row + stock_row[3:]
                 all_data.append(all_data_row)
 
     # Save merged dataset to the file
@@ -33,7 +33,7 @@ def merge_data_files(twitter_file, stock_file, output_file):
 
 if __name__ == "__main__":
     merge_data_files(
-        twitter_file="datasets/training/support/training_twitter.json",
-        stock_file="datasets/training/support/stock_data_04-09-2019.csv",
+        twitter_file="datasets/training/support/tweets_summary.json",
+        stock_file="datasets/training/support/stock_new.csv",
         output_file="datasets/training/prepared/training_data.csv",
     )
